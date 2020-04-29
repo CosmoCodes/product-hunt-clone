98c4f6e35773a0f180f815522b8eb76b0119f839
Ankit Ghildiyal <cosmothecreater@gmail.com>
2020-04-29 06:05:33 +0530

    completed the project

--- product_hunt_clone/urls.py
+++ product_hunt_clone/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path,include
 from products import views as prodViews
 from accounts import views as accViews
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',prodViews.home,name='home'),
     path('accounts/',include('accounts.urls'),name='accounts'),
-]
+    path('products/',include('products.urls'),name='products'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
